3257a2b93d173dd143bb33fcd7a1f6e597cf56e9
Pongchanan <pongchanansriwanna2547@gmail.com>
2024-12-26 13:06:39 +0700

    Add date conversion functionality and integrate with file upload in Data Visualizer

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import streamlit as st
+import pandas as pd
+
+def date_converter(date_col):
+    # Convert to string format and extract the date part
+    result = date_col.dt.strftime('%Y-%m-%d').tolist()
+    return result
 
 st.markdown("<h1 style='text-align: center;'>Data Visualizer</h1>", unsafe_allow_html=True)
 st.markdown("---")
@@ -13,4 +19,8 @@ if files:
     if selected_file:
         option = st.radio("Select Entity Against Date", options=["None", "GPU", "CPU", "MOUSE", "KEYBOARD", "CASING"])
         if option != "None":
-            print(option)
+            for file in files:
+                if file.name in selected_file:
+                    shop_data = pd.read_excel(file, index_col=0)
+                    dates = date_converter(shop_data["DATE"])
+                    print(dates)
